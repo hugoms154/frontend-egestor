c8df01bfa96f65db8745e7f1c7272e5e47c93082
hugo miranda <hugo.miranda.souza@gmail.com>
2020-09-27 06:51:20 -0300

    feat: import csv/txt file |page, components, route

---
+++ src/pages/Import/index.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { FiAlertCircle } from 'react-icons/fi';
+import { useHistory } from 'react-router-dom';
+
+import filesize from 'filesize';
+
+import Header from '../../components/Header';
+import FileList from '../../components/FileList';
+import Upload from '../../components/Upload';
+
+import { Container, Title, ImportFileContainer, Footer } from './styles';
+
+import api from '../../services/api';
+
+interface FileProps {
+  file: File;
+  name: string;
+  readableSize: string;
+}
+
+const Import: React.FC = () => {
+  const [uploadedFiles, setUploadedFiles] = useState<FileProps[]>([]);
+  const history = useHistory();
+
+  async function handleUpload(): Promise<void> {
+    // TODO
+    Promise.all(
+      uploadedFiles.map(async filex => {
+        const { name, file } = filex;
+        const data = new FormData();
+        try {
+          data.append('file', file, name);
+          return await api.post('/employees/import', data);
+        } catch (err) {
+          console.log(err.response.error);
+        }
+      }),
+    ).then(() => history.push('/'));
+  }
+
+  function submitFile(files: File[]): void {
+    // TODO
+    console.log(files);
+    const acceptedFiles = files.map(uploadedFile => ({
+      file: uploadedFile,
+      name: uploadedFile.name,
+      readableSize: filesize(uploadedFile.size),
+    }));
+    console.log(acceptedFiles);
+    setUploadedFiles(acceptedFiles);
+    console.log(uploadedFiles);
+  }
+
+  return (
+    <>
+      <Header size="small" />
+      <Container>
+        <Title>Importar uma transação</Title>
+        <ImportFileContainer>
+          <Upload onUpload={submitFile} />
+          {!!uploadedFiles.length && <FileList files={uploadedFiles} />}
+
+          <Footer>
+            <p>
+              <FiAlertCircle />
+              Permitido apenas arquivos CSV
+            </p>
+            <button onClick={handleUpload} type="button">
+              Enviar
+            </button>
+          </Footer>
+        </ImportFileContainer>
+      </Container>
+    </>
+  );
+};
+
+export default Import;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,10 +3,17 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
+import Import from '../pages/Import';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
+    <Route path="/import" exact component={Import} />
+    {/*
+    /delete
+    /insert
+    /search
+    */}
   </Switch>
 );
 
