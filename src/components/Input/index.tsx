a0bebe0f53925d51c015648f48ec9bcc8f65d8e0
hugo miranda <hugo.miranda.souza@gmail.com>
2020-09-28 01:23:55 -0300

    Adding: input component with unform

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,35 @@
+import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
+import { useField } from '@unform/core';
+
+import { FiAlertCircle } from 'react-icons/fi';
+import { Container, Error } from './styles';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+}
+
+const Input: React.FC<InputProps> = ({ name, ...rest }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { fieldName, defaultValue, registerField, error } = useField(name);
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container isErrored={!!error}>
+      <input ref={inputRef} defaultValue={defaultValue} {...rest} />
+
+      {error && (
+        <Error title={error}>
+          <FiAlertCircle color="#c53030" size={20} />
+        </Error>
+      )}
+    </Container>
+  );
+};
+
+export default Input;
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+import Tooltip from '../Tooltip';
+
+interface ContainerProps {
+  isErrored: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
+  position: relative;
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+  max-width: 700px;
+  height: 56px;
+  padding: 0 10px;
+  border-left: 2.5px solid #f0f2f5;
+  &:focus-within {
+    border-left: 2.5px solid #10bb77;
+  }
+
+  div {
+    position: absolute;
+    right: 10px;
+  }
+
+  & + div {
+    margin-top: 10px;
+  }
+
+  input {
+    width: 100%;
+    padding-right: 30px;
+    background: transparent;
+    border: 0;
+
+    &::placeholder {
+    }
+  }
+
+  input:last-child {
+    padding: 0;
+    color: #747474;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background-color: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
+  }
+`;
